210fefca19b2908d402d53bfc283a1f3886b6767
Izzykilian-source <mohamedizzykilian@gmail.com>
2025-01-10 21:47:04 +0800

    Delete coba uy directory

--- coba uy/scripts/app.js
+++
@@ -1,8 +0,0 @@
-// Log ke konsol saat file dimuat
-console.log("Mock Test Report Loaded");
-
-// Menambahkan tahun dinamis di footer
-const footerElement = document.querySelector("footer p");
-if (footerElement) {
-  footerElement.innerText += ` | ${new Date().getFullYear()}`;
-}
